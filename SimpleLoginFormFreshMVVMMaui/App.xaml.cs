a12382521f5eff167832098e3bad129430cb5a9f
Bharat Kolla <bharatkolla11@gmail.com>
2022-01-25 19:24:24 -0600

    Add project files.

---
+++ SimpleLoginFormFreshMVVMMaui/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace SimpleLoginFormFreshMVVMMaui;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ SimpleLoginFormFreshMVVMMaui/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace SimpleLoginFormFreshMVVMMaui;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
